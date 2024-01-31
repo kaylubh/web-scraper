53fb3915293a60c8872921f09a791cbd76916e87
Caleb Hemphill <calebhemp@gmail.com>
2024-01-31 00:13:16 -0500

    Add docstrings

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 def parse_article_markup(raw_article_document):
     """
-    
+    Using Beautiful Soup, parses an HTML document of a Wikipedia article page and returns the part of the document which contains the main article.
     """
 
     # parse the document as html
@@ -16,7 +16,7 @@ def parse_article_markup(raw_article_document):
 
 def extract_citations_needed_paragraphs(article_document):
     """
-    
+    Given an HTML document of a Wikipedia article parsed with Beautiful Soup, extracts and returns a list of the text content of all paragraphs which have a "[citation needed]" annotation.
     """
 
     # extract all paragraphs in the article
@@ -37,7 +37,7 @@ def extract_citations_needed_paragraphs(article_document):
 
 def get_citations_needed_count(article_url):
     """
-    
+    Given a URL to a Wikipedia article, returns the number of times a paragraph has been annotated with "[citation needed]" in the article.
     """
 
     raw_article_document = requests.get(article_url)
@@ -48,22 +48,22 @@ def get_citations_needed_count(article_url):
 
 def get_citations_needed_report(article_url):
     """
-    
+    Given a URL to a Wikipedia article, returns a string of the paragraphs which have been annotated with "[citation needed]" in the article.   
     """
 
     raw_article_document = requests.get(article_url)
     parsed_article_document = parse_article_markup(raw_article_document.text)
     paragraphs_citations_needed = extract_citations_needed_paragraphs(parsed_article_document)
 
-    citations_needed_report = []
+    citations_needed_report = ""
     for paragraph in paragraphs_citations_needed:
-        citations_needed_report.append(f"\n***Citation Needed***\n{paragraph}")
+        citations_needed_report += (f"\n***Citation Needed***\n{paragraph}\n")
 
     return citations_needed_report
 
 def citations_needed_app():
     """
-    
+    Prompts the user for a URL path to a Wikipedia article to check for paragraphs that need citations. Provides the user print statements of the count of citations needed and the content of the paragraphs which need citations.
     """
 
     print("\nInput the full URL path for a Wikipedia article to be checked for citations needed.\nThe URL MUST be a Wikipedia article and be a valid path.\nExample: https://en.wikipedia.org/wiki/Potaton\n")
@@ -83,9 +83,7 @@ def citations_needed_app():
 
             citations_needed_report = get_citations_needed_report(article_url_to_check)
 
-            for citation_needed in citations_needed_report:
-                print(citation_needed)
-
+            print(citations_needed_report)
             print("\nExiting...\nRun again to check a new article\n")
         
         else:

65ba15fc64fa87e5410dcab5275dfd036c41d236
Caleb Hemphill <calebhemp@gmail.com>
2024-01-30 19:53:13 -0500

    Add function to parse article markup

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,25 +1,45 @@
 import requests
+from bs4 import BeautifulSoup
+
+def parse_article_markup(raw_article_document):
+    """
+    
+    """
+
+    # parse the document as html
+    soup = BeautifulSoup(raw_article_document, "html.parser")
+
+    # extract the article content from the document
+    article_document = soup.select("div.mw-content-ltr")
+
+    return article_document
+
+def extract_citations_needed():
+    """
+    
+    """
+
+    # extract all paragraphs in the article
+    article_para
 
 def get_citations_needed_count(wikipedia_article_url):
     """
     
     """
 
-    raw_article_page = requests.get(wikipedia_article_url)
+    raw_article_document = requests.get(wikipedia_article_url)
+    parsed_article_document = parse_article_markup(raw_article_document.text)
 
 
-
-    return raw_article_page
+    return parsed_article_document
 
 def get_citations_needed_report(wikipedia_article_url):
     """
     
     """
 
-def extract_citations_needed(raw_article_page):
-    """
-    
-    """
+
+
 
 
 
